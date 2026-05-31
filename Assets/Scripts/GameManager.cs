c604e8893805bc03483d050f9e3270f7a301a607
Ryan Thomas <ryanthomasj15@gmail.com>
2026-05-31 14:21:05 +0100

    gamemanager class

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace ryathom.RunTheNet
+{
+    public class GameManager : MonoBehaviour
+    {
+        public void Start()
+        {
+            Debug.Log("Hello world.");
+        }
+    }
+}
