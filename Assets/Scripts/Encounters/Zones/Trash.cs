721078941f8cdb0fc536932b28dfd4a3aff985e3
Ryan Thomas <ryanthomasj15@gmail.com>
2026-06-19 15:45:00 +0100

    rest of fix

--- Assets/Scripts/Encounters/Zones/Trash.cs
+++ Assets/Scripts/Encounters/Zones/Trash.cs
@@ -1,9 +1,23 @@
+using ryathom.RunTheNet.Encounters.Cards;
+
 namespace ryathom.RunTheNet.Encounters.Zones
 {
     public class Trash : Zone
     {
         public Trash() : base()
         {
+        }
+
+        public override void AddCard(Card card)
+        {
+            base.AddCard(card);
+
+            card.Activate();
+            
+            if (card is Program program)
+            {
+                program.ResetStrength();
+            }
         }
     }
 }
