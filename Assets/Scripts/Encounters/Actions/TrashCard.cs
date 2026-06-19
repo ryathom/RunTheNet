bd2e1bbb967ffbaddcafd92ac3e4e6d619d5c293
Ryan Thomas <ryanthomasj15@gmail.com>
2026-06-19 16:09:51 +0100

    Clean up debug logs

--- Assets/Scripts/Encounters/Actions/TrashCard.cs
+++ Assets/Scripts/Encounters/Actions/TrashCard.cs
@@ -19,18 +19,14 @@ namespace ryathom.RunTheNet.Encounters.Actions
         {
             if (Card == null) yield break;
 
-            Debug.Log("Trashing " + Card.Name + " which is a " + Card.GetType().Name);
-
             if (Card is Program)
             {
-                Debug.Log("Here because it's a program");
                 yield return EncounterManager.Instance.Actions.ExecuteImmediate(new ChangeZone(Card, EncounterManager.Instance.Runner.Trash));
             } else if (Card is Hardware)
             {
                 Debug.LogError("Hardware not expected to be trashed in prototype");
             } else
             {
-                Debug.Log("Here because it's not a program");
                 yield return EncounterManager.Instance.Actions.ExecuteImmediate(new ChangeZone(Card, EncounterManager.Instance.CorpTrash));
             }
 
