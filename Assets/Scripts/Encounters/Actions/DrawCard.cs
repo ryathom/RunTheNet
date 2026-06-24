ae434a69d48e72fe9b9771a74cabe2781e904f17
Ryan Thomas <ryanthomasj15@gmail.com>
2026-06-24 14:40:04 +0100

    auto gc

--- Assets/Scripts/Encounters/Actions/DrawCard.cs
+++ Assets/Scripts/Encounters/Actions/DrawCard.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using ryathom.RunTheNet.Encounters.Player;
+using ryathom.RunTheNet.Encounters.Cards;
 
 namespace ryathom.RunTheNet.Encounters.Actions
 {
@@ -11,9 +12,29 @@ namespace ryathom.RunTheNet.Encounters.Actions
         public IEnumerator Execute()
         {
             runner = EncounterManager.Instance.Runner;
-            if (runner.Repository.Cards.Count <= 0) yield break;
+            if (runner.Repository.Cards.Count <= 0)
+            {
+                if (runner.Trash.Cards.Count > 0)
+                {
+                    yield return GarbageCollection();
+                    yield return new WaitForSeconds(0.5f);
+                } else
+                {
+                    yield break;
+                }
+            }
             
             yield return EncounterManager.Instance.Actions.ExecuteImmediate(new ChangeZone(runner.Repository.Cards[0], runner.Hand));
+        }
+
+        public IEnumerator GarbageCollection()
+        {
+            while(EncounterManager.Instance.Runner.Trash.Cards.Count > 0)
+            {
+                yield return EncounterManager.Instance.Actions.ExecuteImmediate(new ChangeZone(EncounterManager.Instance.Runner.Trash.Cards[0], EncounterManager.Instance.Runner.Repository));
+            }            
+
+            yield return EncounterManager.Instance.Actions.ExecuteImmediate(new ShuffleRepo());
         }
     }
 }
