877edfe44f84a06d83bfcc78c25457607486f617
Ryan Thomas <ryanthomasj15@gmail.com>
2026-07-11 11:34:23 +0100

    clean up ui manager

--- Assets/Scripts/Run/RunUIManager.cs
+++ Assets/Scripts/Run/RunUIManager.cs
@@ -10,31 +10,15 @@ namespace ryathom.RunTheNet.Run
         [SerializeField] private TextMeshProUGUI creditsText;
         [SerializeField] private RepoBrowser repoBrowser;
 
-        [SerializeField] private List<Button> EncounterButtons;
 
         private void Start()
         {
             ShowRepoBrowser(false);
-            ShowValidEncounters();
         }
 
         private void Update()
         {
             creditsText.text = "Credits: " + RunManager.Instance.Credits;
-        }
-
-        public void ShowValidEncounters()
-        {
-            for (int i = 0; i < EncounterButtons.Count; i++)
-            {
-                if (i == RunManager.Instance.ProgressLevel)
-                {
-                    EncounterButtons[i].GetComponent<Image>().color = Color.white;
-                } else
-                {
-                    EncounterButtons[i].GetComponent<Image>().color = Color.grey;
-                }
-            }
         }
 
         public void ShowRepoBrowser(bool enabled)
